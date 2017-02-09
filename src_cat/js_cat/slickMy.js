891701c0a1849dd609ffc4312f4e4883c694d9c4
eksana <ser.vajnrub@yandex.ru>
2017-02-09 12:35:33 +0300

    add cat.html

---
+++ src_cat/js_cat/slickMy.js
@@ -0,0 +1,34 @@
+
+    $(document).ready(function(){
+      $('.center').slick({
+      	autoplay:true,
+      	
+      	arrows: true,
+
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+       
+    });
+ 
